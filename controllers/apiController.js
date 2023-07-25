6be329f29f964e8070fc5cccf99b57401e9acf1a
Raddzor01 <danil.garin.04@gmail.com>
2023-07-25 19:23:17 +0300

    Error handling

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,6 +1,8 @@
 import { nanoid } from 'nanoid';
 import db from '../db/db-init.js';
 
+const MAX_ATTEMPTS = 100;
+
 async function saveLink(req, res) {
     try {
         const data = req.body.data;
@@ -10,7 +12,26 @@ async function saveLink(req, res) {
             return res.send(JSON.stringify(response));
         }
 
-        const id = nanoid(5);
+        let id;
+        let attempts = 0;
+
+        while (attempts < MAX_ATTEMPTS) {
+            id = nanoid(5);
+
+            const countResult = await db.query('SELECT EXISTS(SELECT id FROM links WHERE id = $1)', [id]);
+            const recordExists = countResult.rows[0].exists;
+
+            if (!recordExists) {
+                break;
+            }
+            attempts++;
+        }
+
+        if (attempts === MAX_ATTEMPTS) {
+            throw new Error('Unable to find a unique id.');
+        }
+
+
         await db.query('INSERT INTO links (id, url) VALUES ($1, $2)', [id, data]);
 
         const response = { message: id };
@@ -23,6 +44,6 @@ async function saveLink(req, res) {
 
 function isURL(str) {
     return /^(https?:\/\/)?\w+(\.\w+)+.*$/.test(str);
-  }
+}
 
 export default saveLink;
--- public/js/script.js
+++ public/js/script.js
@@ -1,33 +1,34 @@
 const resultOutput = document.querySelector('#result p');
 const errOutput = document.querySelector('#err-result p');
 
-function submitData() {
+async function submitData() {
     const inputValue = document.getElementById('textInput').value;
 
-    fetch('/api/data', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ data: inputValue })
-    })
-        .then(response => response.json())
-        .then(data => {
-            if (data.message === undefined) {
-                let outputText = resultOutput.textContent;
-                resultOutput.textContent = '';
-                errOutput.textContent = 'Invalid link!';
-                setTimeout(() => {
-                    errOutput.textContent = '';
-                    resultOutput.textContent = outputText;
-             }, 3000);
-            } else {
-                resultOutput.textContent = `Your short link is: ${window.location.href}${data.message}`;
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
+    try {
+        const response = await fetch('/api/data', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ data: inputValue })
         });
+
+        const data = await response.json();
+
+        if (data.message === undefined) {
+            let outputText = resultOutput.textContent;
+            resultOutput.textContent = '';
+            errOutput.textContent = 'Invalid link!';
+            setTimeout(() => {
+                errOutput.textContent = '';
+                resultOutput.textContent = outputText;
+            }, 3000);
+        } else {
+            resultOutput.textContent = `Your short link is: ${window.location.href}${data.message}`;
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
 
 resultOutput.addEventListener('click', () => {
